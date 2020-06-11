b5d540b95fd4640b4ee0ccf4c3ae24c86492eeb6
朱明明 <478254406@qq.com>
2020-06-11 22:33:37 +0800

    java源码学习的第一次提交

---
+++ javaSourceLearn/src/cn/zmm/test/hashMap/test.java
@@ -0,0 +1,14 @@
+package cn.zmm.test.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+    public static void main(String[] args) {
+        Map<String,Double>hashMap=new HashMap<>();
+        hashMap.put("p1",0.1);
+        hashMap.put("p2",0.2);
+        hashMap.put("p3",0.3);
+        hashMap.put("p4",0.4);
+    }
+}
